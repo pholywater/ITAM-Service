8578376353ac82c42c7243f5d60169005e083018
pholywater <pholywater0505@gmail.com>
2023-10-24 16:28:06 +0900

    itam

---
+++ src/main/java/hmm/itam/ItamApplication.java
@@ -0,0 +1,13 @@
+package hmm.itam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hmm/itam/ItamApplicationTests.java
@@ -0,0 +1,13 @@
+package hmm.itam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
