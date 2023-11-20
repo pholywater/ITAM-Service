172b0bcd42ac90a9f41014b49881ef0417cc857f
pholywater <pholywater0505@gmail.com>
2023-11-20 15:34:39 +0900

    itam

--- src/main/java/hmm/itam/controller/AssetController.java
+++ src/main/java/hmm/itam/controller/AssetController.java
@@ -2,11 +2,15 @@ package hmm.itam.controller;
 
 import hmm.itam.service.AssetService;
 import hmm.itam.vo.AssetVo;
+import hmm.itam.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -21,4 +25,29 @@ public class AssetController {
         model.addAttribute("list", assetList);
         return "itam/assetList"; // 실제 HTML 경로
     }
+
+    @GetMapping("/assetAdd") // 자산 등록 화면
+    public String toAssetaddPage()
+    {
+        return "/itam/assetAdd";
+    }
+
+    @PostMapping("/assetAdd") // 자산 등록 입력 처리
+    public String assetadd(AssetVo assetVo) {
+        try {
+            AssetService.assetAdd(assetVo);
+        } catch (DuplicateKeyException e) {
+            return "redirect:/assetadd?error_code=-1";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/assetadd?error_code=-99";
+        }
+        return "redirect:/itam/assetResult"; // 자산 등록 후 보여질 화면
+    }
+    @GetMapping("/assetResult") // 자산 등록 후 화면
+    public String assetResult() {
+        return "/itam/assetResult";
+    }
+
+
 }
