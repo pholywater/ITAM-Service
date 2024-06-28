4405f5132c40ecfb328d271987cfcf36bfddaceb
pholywater <pholywater0505@gmail.com>
2024-06-29 02:11:09 +0900

    itam

--- src/main/java/hmm/itam/service/HistoryService.java
+++ src/main/java/hmm/itam/service/HistoryService.java
@@ -24,29 +24,32 @@ public class HistoryService {
         /*AssetMapper.insertModel(assetVo);*/
     }
 
-    /*관리번호 조회*/
-    public List<HistoryVo> historyAssetSearch(String search) {
-        return HistoryMapper.getHistoryAssetNumber(search);
-    }
-
-    /*이름 조회*/
-    public List<HistoryVo> historyNameSearch(String search) {
-        return HistoryMapper.getHistoryMemberName(search);
-    }
-
-    /*사번 조회*/
-    public List<HistoryVo> historyIdSearch(String search) {
-        return HistoryMapper.getHistoryMemberId(search);
-    }
-
-    /*간편 조회*/
-    public List<HistoryVo> historySearch(String search, String searchType) {
-        return HistoryMapper.getHistorySearch(search, searchType);
-    }
-
     /*장비 등록 후 조회*/
     public List<HistoryVo> historyAddResult(String search) {
         return HistoryMapper.getHistoryAssetNumber(search);
     }
 
+    /*이력 관리 상세 조회(동적쿼리로 변경 - 변수가 2개 필요했었음)*/
+    public List<HistoryVo> historySearch(String search, String searchType) {
+        return HistoryMapper.getHistorySearch(search, searchType);
+    }
+
+
+
+
+    /* 동적 쿼리문으로 변경 후 필여 없어짐
+     *//*관리번호 조회*//*
+    public List<HistoryVo> historyAssetSearch(String search) {
+        return HistoryMapper.getHistoryAssetNumber(search);
+    }
+
+    *//*이름 조회*//*
+    public List<HistoryVo> historyNameSearch(String search) {
+        return HistoryMapper.getHistoryMemberName(search);
+    }
+
+    *//*사번 조회*//*
+    public List<HistoryVo> historyIdSearch(String search) {
+        return HistoryMapper.getHistoryMemberId(search);
+    }*/
 }
