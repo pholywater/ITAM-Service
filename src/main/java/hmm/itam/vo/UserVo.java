6ea1c33d74299542143097afbb8c952f34147b35
pholywater <pholywater0505@gmail.com>
2024-07-04 01:08:54 +0900

    엔터 Submit 방지

--- src/main/java/hmm/itam/vo/UserVo.java
+++ src/main/java/hmm/itam/vo/UserVo.java
@@ -4,11 +4,11 @@ import lombok.Data;
 
 @Data
 public class UserVo {
-
     private Long idx;
     private String hmmId;
     private String name;
     private String comment;
     private String department;
     private String password;
+    private String authority;
 }
