12523a97b03496d1c35f333b87b9dcf00f8e80b6
pholywater <pholywater0505@gmail.com>
2024-11-22 16:55:28 +0900

    직원 정보 등록 및 검색, 수정, 리스트 조회

--- src/main/java/hmm/itam/mapper/MemberMapper.java
+++ src/main/java/hmm/itam/mapper/MemberMapper.java
@@ -1,5 +1,6 @@
 package hmm.itam.mapper;
 
+import hmm.itam.vo.AssetVo;
 import hmm.itam.vo.MemberVo;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -7,11 +8,16 @@ import java.util.List;
 
 @Mapper
 public interface MemberMapper {
-    List<MemberVo> getMemberList();
 
     void insertMember(MemberVo memberVo); // 신규직원 등록
 
-    List<MemberVo> getDepartmentList();
+    List<MemberVo> getDepartmentList(); // datalist 자동완성 부서 검색 리스트
+
+    List<MemberVo> getMemberList(); // datalist 자동완성 직원 이름 검색 리스트
+
+    MemberVo getMemberByMemberId(String memberId); // 직원번호로 검색
+
+    void updateMember(MemberVo memberVo); // 직원 정보 수정
 
     List<MemberVo> getHeaderDepartmentList();
 }
--- src/main/java/hmm/itam/service/MemberService.java
+++ src/main/java/hmm/itam/service/MemberService.java
@@ -1,31 +1,48 @@
 package hmm.itam.service;
 
 import hmm.itam.mapper.MemberMapper;
+import hmm.itam.vo.AssetVo;
 import hmm.itam.vo.MemberVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class MemberService {
     @Autowired
-    private static MemberMapper memberMapper;
+    private MemberMapper MemberMapper;
 
-    public List<MemberVo> getMemberList() {
-
-        return memberMapper.getMemberList();
-    }
 
     public void memberAdd(MemberVo memberVo) {
-        memberMapper.insertMember(memberVo);
+        MemberMapper.insertMember(memberVo);
     }
 
+
+    /*부서 리스트 자동완성*/
     public List<MemberVo> getDepartmentList() {
-        return memberMapper.getDepartmentList();
+        return MemberMapper.getDepartmentList();
     }
 
-    public static List<MemberVo> getHeaderDepartmentList() {
-        return memberMapper.getHeaderDepartmentList();
+    /*멤버 리스트 자동완성*/
+    public List<MemberVo> getMemberList() {
+        return MemberMapper.getMemberList();
+    }
+
+    /*멤버 조회*/
+    public MemberVo memberSearch(String memberId) {
+        return MemberMapper.getMemberByMemberId(memberId);
+    }
+
+    /*멤버 정보 수정*/
+    public void modifyInfo(MemberVo memberVo) {
+        MemberMapper.updateMember(memberVo);
+    }
+
+
+    public List<MemberVo> getHeaderDepartmentList() {
+        return MemberMapper.getHeaderDepartmentList();
     }
 }
